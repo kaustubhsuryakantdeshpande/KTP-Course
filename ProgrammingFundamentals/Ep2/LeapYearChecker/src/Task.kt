e04cce65bd7e379d7f8393b55e32501d97976bf8
Kaustubh <ksd.tcet@gmail.com>
2024-09-21 00:32:38 +0530

    Attempt to commit some task info thing not sure what it is

--- ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
+++ ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
@@ -1,6 +1,7 @@
 import com.faangx.ktp.basics.LeapYearCheckerMiniApp
 
 fun isLeapYear(year: Int): Boolean {
+
     return if ( year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
         true
     }else{
