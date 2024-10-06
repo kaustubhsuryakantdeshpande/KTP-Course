becb6ca3649a752689e720af4c1dfb177e74c535
Kaustubh <ksd.tcet@gmail.com>
2024-10-06 22:20:41 +0530

    pattern 5 and binomial update

--- ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
@@ -1,19 +1,34 @@
 import com.faangx.ktp.basics.BinomialExpansionMiniApp
 
 fun printBinomialExpansion(n: Int) {
-    TODO()
+    val a = 'a'
+    val b = 'b'
+    val terms = mutableListOf<String>()
+
+    for (k in 0..n) {
+        val coefficient = combinationsOf(n.toLong(), k.toLong())
+        val term = when {
+            k == 0 -> "$a^$n"
+            k == n -> "$b^$n"
+            else -> "${coefficient}${a}^${n - k}${b}^$k"
+        }
+        terms.add(term)
+    }
+
+    val expansion = terms.joinToString(" + ")
+    println("($a + $b)^$n = $expansion")
 }
 
 fun factorialOf(x: Long): Long {
-    TODO()
+    return if (x == 0L) 1L else x * factorialOf(x-1)
 }
 
 fun permutationsOf(n: Long, r: Long): Long {
-    TODO()
+    return factorialOf(n) / factorialOf(n -r)
 }
 
 fun combinationsOf(n: Long, r: Long): Long {
-    TODO()
+    return factorialOf(n) / (factorialOf(r) * factorialOf(n - r))
 }
 
 fun main() {
