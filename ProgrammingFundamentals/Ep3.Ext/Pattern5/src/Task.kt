9687584626a6ff19628f6465cff02ad0bdb9c360
Kaustubh <ksd.tcet@gmail.com>
2024-10-13 22:38:58 +0530

    added episode 3 extended pattern 5

--- ProgrammingFundamentals/Ep3.Ext/Pattern5/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern5/src/Task.kt
@@ -22,11 +22,11 @@ fun printPattern(lines: Int, char1: Char, char2: Char) {
     // Lower part of the pattern
     repeat(lines / 2) { i ->
         repeat(lines / 2 - i - 1) { print(" ") }
-        repeat(lines + 2 * i -4) { j ->
-            if ( j == 3 - i - 1 || j == lines - 1 - ((lines / 2 ) - i - 1)  ) {
-                print(char2)
-            } else {
-                print(char1)
+        repeat(2 * i + 3) { j ->
+            when (j){
+                0, 2 * i + 2 -> print(char2)
+                i + 1 -> print(char1)
+                else -> print(' ')
             }
         }
         println()
--- ProgrammingFundamentals/Ep4/LCM-HCF/src/Task.kt
+++ ProgrammingFundamentals/Ep4/LCM-HCF/src/Task.kt
@@ -1,11 +1,27 @@
 import com.faangx.ktp.basics.LcmHcfCalculatorMiniApp
 
-fun getLCM(x: Int, y: Int): Int {
-    TODO()
+fun getLCM(x: Int, y: Int): Int { // common division approach
+    val a = if (x > y ) x else y
+    val b = if ( x < y ) x else y
+
+    var multiple = a
+    while (multiple % b != 0){
+        multiple = multiple + a
+    }
+
+    return multiple
 }
 
-fun getHCF(x: Int, y: Int): Int {
-    TODO()
+fun getHCF(x: Int, y: Int): Int { //long division
+    var a = x
+    var b = y
+
+    while ( b != 0){
+        val remainder = a % b
+        a = b
+        b = remainder
+    }
+    return  a //since remainder has to come out zero in end we return a only
 }
 
 fun main() {
@@ -14,3 +30,12 @@ fun main() {
         ::getHCF
     )
 }
+
+/* Comments : There is some issue when using debugger on
+inserting values in mini app
+ */
+
+/*
+alias for LCM
+LCM = a * b / HCF
+ */
