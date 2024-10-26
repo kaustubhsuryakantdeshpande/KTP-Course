d29b56bfc5407b8d8f22e360e1a9c882072d0e9e
Kaustubh <ksd.tcet@gmail.com>
2024-10-26 22:19:27 +0530

    Done wiht EP 4 Extended

--- ProgrammingFundamentals/Ep4.Ext/DigitsPowerSum/src/Task.kt
+++ ProgrammingFundamentals/Ep4.Ext/DigitsPowerSum/src/Task.kt
@@ -1,5 +1,15 @@
 fun getDigitsPowerSum(num: Int): Int {
-    TODO()
+    return koshish(num)
+}
+
+fun koshish(num: Int): Int{
+    var ulta = 0
+    var ultaKaSquare = 0
+    while(num > 0){
+        ulta = num % 10
+        ultaKaSquare = ultaKaSquare + (ulta * ulta)
+    }
+    return ultaKaSquare
 }
 
 fun main() {
