83875119c101978c52c9b0eb32c19f28a12518b9
Kaustubh <ksd.tcet@gmail.com>
2024-10-26 21:58:43 +0530

    Done with EP 4

--- ProgrammingFundamentals/Ep4/StringCaseConvertor/src/Task.kt
+++ ProgrammingFundamentals/Ep4/StringCaseConvertor/src/Task.kt
@@ -1,31 +1,93 @@
 import com.faangx.ktp.basics.StringCaseConvertorMiniApp
 
 fun convertToSentenceCase(text: String): String {
-    TODO()
+    var capitlize = true // since every new sentence starts with capital
+    var requireSpace = false
+
+    return buildString {
+        for (char in text){
+            if (requireSpace){
+                if (char != ' ') append(' ')
+                requireSpace = false
+            }
+            if (isAlphabet(char) && capitlize){
+                append(convertToUppercase(char))
+                capitlize = false
+            } else {
+                append(char)
+            }
+            if (char in listOf('.', '!', '?')){
+                capitlize = true
+                requireSpace = true
+            }
+        }
+    }
+}
+
+fun isAlphabet(char: Char): Boolean {
+    return char in 'a'..'z' || char in 'A'..'Z'
+
 }
 
 fun convertToTitleCase(text: String): String {
-    return "TODO()"
+    var capitalize = true
+
+    return buildString {
+        for (char in text){
+            if (char != ' '){ //ignore space
+                if (capitalize){ // capitalize if needed
+                    append(convertToUppercase(char))
+                    capitalize = false
+                } else{
+                    append(char)
+                }
+            }
+            if (char in listOf('.', '!', '?', ':', ';', '-', ' ', ',' ))
+                capitalize = true
+        }
+    }
 }
 
 fun convertToUppercase(text: String): String {
-    return "TODO()"
+    return buildString {
+        for (char in text) {
+            append(convertToUppercase(char))
+        }
+    }
 }
 
 fun convertToLowercase(text: String): String {
-    return "TODO()"
+    return buildString {
+        for (char in text){
+            append(convertToLowercase(char))
+        }
+    }
 }
 
 fun convertToSnakeCase(text: String): String {
-    return "TODO()"
+    return buildString {
+        for (char in text ){
+            if (char == ' '){
+                append('_')
+            } else {
+                append(char)
+            }
+        }
+    }
 }
 
 fun convertToUppercase(char: Char): Char {
-    TODO()
+    return when(char){
+        in 'a'..'z' -> char.uppercaseChar()
+        else -> char
+    }
 }
-
+// Yeh convert to lower and upper dono ko alag naming dena chahiye tha confusioni create kar raha hai especially shuru shuru me toh char me dhyan hi ni jata
 fun convertToLowercase(char: Char): Char {
-    TODO()
+    return when(char){
+        in 'A'..'Z' -> char.lowercaseChar()
+        else -> char
+    }
 }
 
 fun main() {
--- ProgrammingFundamentals/Ep4/StringStatsCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep4/StringStatsCalculator/src/Task.kt
@@ -1,39 +1,97 @@
 import com.faangx.ktp.basics.StringStatsCalculatorMiniApp
 
 fun calcNoOfChars(text: String): Int {
-    TODO()
+    return text.length
 }
 
 fun calcNoOfAlphabets(text: String): Int {
-    return 0
+    var count = 0
+    for (i in text){
+        if ( i in 'A'..'Z' ||i in 'a'.. 'z'){
+            count = count + 1
+        }
+    }
+    return count
 }
 
-fun calcNoOfVowels(text: String): Int {
-    return 0
+fun calcNoOfVowels(text: String): Int { //good way to teach lists
+    var count = 0
+    for ( c in text){
+        if (c in listOf('a','e','i','o','u','A','E','I','O','U'))
+            count = count + 1
+    }
+    return count
+}
+
+fun isAlphabet(c: Char): Boolean {
+    return c in 'a'..'z' || c in 'A'..'Z'
 }
 
 fun calcNoOfConsonants(text: String): Int {
-    return 0
+    /*
+    var count = 0
+    for ( c in text){
+        if (isAlphabet(c) c !in listOf('a','e','i','o','u','A','E','I','O','U'))
+            count = count + 1
+    }
+    return count
+     */
+    return calcNoOfAlphabets(text) - calcNoOfVowels(text)
 }
 
 fun calcNoOfDigits(text: String): Int {
-    return 0
+    var count = 0
+    for ( c in text){
+        if (c in '0'..'9')
+            count = count + 1
+    }
+    return count
 }
 
 fun calcNoOfSpecialChars(text: String): Int {
-    return 0
+    // since creating a list of all special characters will be tedious so,
+    return text.length - (calcNoOfAlphabets(text) + calcNoOfSpaces(text) + calcNoOfDigits(text))
 }
 
 fun calcNoOfSpaces(text: String): Int {
-    return 0
+    var count = 0
+    for ( c in text){
+        if (c in listOf(' ', '\n', '\t'))
+            count = count + 1
+    }
+    return count
 }
 
 fun calcNoOfWords(text: String): Int {
-    return 0
+    var count = 0
+    var nonPunctuationCharFound = false
+    for ( c in text){
+        if (c !in listOf(' ', '\t', '\n', '.', ',', '?', '!')){
+            nonPunctuationCharFound =true
+        } else if (nonPunctuationCharFound){
+            nonPunctuationCharFound = false
+            count = count + 1
+        }
+    }
+    if (nonPunctuationCharFound) count = count + 1
+
+    return count
 }
 
 fun calcNoOfSentences(text: String): Int {
-    return 0
+    var count = 0
+    var nonPunctuationCharFound = false
+    for ( c in text){
+        if (c !in listOf(' ', '\t', '\n', '.', ',', '?', '!')){
+            nonPunctuationCharFound =true
+        } else if (c in listOf('.', '!', '?') && nonPunctuationCharFound){
+            nonPunctuationCharFound = false
+            count = count + 1
+        }
+    }
+    if (nonPunctuationCharFound) count = count + 1
+
+    return count
 }
 
 fun main() {
